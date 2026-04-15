f5389dbfa9af847c77d8fc0566f0a99679d9bf5f
Thiencc03-07-2006 <cct20062250.@gmail.com>
2026-04-15 22:03:00 +0700

    bai8

---
+++ src/main/java/com/validation/bai1/controller/UserController.java
@@ -0,0 +1,16 @@
+package com.validation.bai1.controller;
+
+import com.validation.bai1.modal.dto.AddressDto;
+import jakarta.validation.Valid;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+@RestController
+@RequestMapping("/api/address")
+public class UserController {
+
+    @PostMapping("/update")
+    public ResponseEntity<String> updateAddress(@Valid @RequestBody AddressDto addressDto) {
+        return ResponseEntity.ok("Cập nhật địa chỉ thành công!");
+    }
+}
